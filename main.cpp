6b27a625351e7775bcef80cb19929b112d339fac
RoseAnnAtienza <54896492+AnnAtienza@users.noreply.github.com>
2019-11-14 17:13:31 +0700

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,72 +1,80 @@
 #include <iostream>
-#include <stdlib.h>
-
 
 using namespace std;
+int inputarrSize();
+void getBasicinfo(string[], string[], int[], int[], string[], string[], string[]);
+void disPlayarray(string[], string[], int[], int[], string[], string[], string[]);
 
-    int inputArrSize();
-    void inputArray(int[]);
-    void displayArray(int[]);
-    void selectArray (int[]);
-    int arrSize;
+
+int arrSize;
 
 int main()
 {
-
-    int new_age[arrSize];
-    inputArray (new_age);
-    displayArray(new_age);
+    string firstname [10];
+    string lastname[10] ;
+    int age[10];
+    int contact[10];
+    string email[10];
+    string address[10];
+    string username[10];
+    getBasicinfo(firstname, lastname, age, contact, email, address, username);
+    disPlayarray(firstname, lastname, age, contact, email, address, username);
 
     return 0;
 }
-int inputArrSize()
+
+int inputarrSize()
 {
-  cout << "Enter Array size 3-4: " ;
-  cin>> arrSize;
+    int age [arrSize];
 
-  return arrSize;
-  }
+    cout <<" Enter Array Size:";
+    cin >> arrSize;
 
-void inputArray(int age[]){
-
-  inputArrSize();
-  for (int i=0; i<= arrSize-1; i++){
-    cout << "Enter a value for position "<< i+1<< ":" ;
-    cin>> age[i];
-  }
+    return arrSize;
 }
 
-void displayArray(int age[]){
-
-    system ("CLS");
-
-  for (int i=0; i<= arrSize-1; i++){
-    cout << "The value at position "<< i+1<< " is :" << age[i]<< endl;
-
-  }
-  selectArray (age);
-}
-void selectArray (int age[])
+void getBasicinfo(string firstname[], string lastname[], int age[], int contact[], string email[], string address[], string username[])
 {
-    int choice;
+    inputarrSize();
+    for (int i=0;i<arrSize;i++){
+    cout <<" Firstname "<< ":";
+    cin >> firstname[i];
+    }
 
-     cout << "Select Array that you want to call :" << endl;
-     cin>> choice;
+     for (int i=0;i<arrSize;i++){
+    cout <<" Lastname "<< ":";
+    cin >> lastname[i];
+    }
 
-     if (choice==1)
-     {
-          cout << "The value at position 1 is :" << age[0]<< endl;
-     }
-       if (choice==2)
-     {
-          cout << "The value at position 2 is :" << age[1]<< endl;
-     }
-       if (choice==3)
-     {
-          cout << "The value at position 3 is :" << age[2]<< endl;
-     }  if (choice==4)
-     {
-          cout << "The value at position 4 is :" << age[3]<< endl;
-     }
+    for (int i=0;i<arrSize;i++){
+    cout <<" Age "<< ":";
+    cin >> age[i];
+    }
 
+    for (int i=0;i<arrSize;i++){
+    cout <<" Contact "<< ":";
+    cin >> contact[i];
+    }
+
+    for (int i=0;i<arrSize;i++){
+    cout <<" E-mail "<< ":";
+    cin >> email[i];
+    }
+
+    for (int i=0;i<arrSize;i++){
+    cout <<" Address "<< ":";
+    cin >> address[i];
+    }
+
+    for (int i=0;i<arrSize;i++){
+    cout <<" Username "<< ":";
+    cin >> username[i];
+    }
+}
+
+void disPlayarray(string firstname[], string lastname[], int age[], int contact[], string email[], string address[], string username[]){
+
+    for (int i=0;i<arrSize;i++){
+    cout << firstname [i]<< "|" << lastname[i]<< "|" << age[i]<< "|" << contact[i]<< "|" << email[i]<< "|" << address[i]<< "|" << username[i]<<endl ;
+    }
 }
